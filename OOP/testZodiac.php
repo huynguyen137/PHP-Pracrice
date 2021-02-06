7f222722e61357badb4331630d4ad23dc9f41175
huynguyen137 <30463880+huynguyen137@users.noreply.github.com>
2021-02-06 17:51:03 +0700

    Small changes
    Change where the result should be placed.

--- OOP/testZodiac.php
+++ OOP/testZodiac.php
@@ -5,22 +5,24 @@
 	<link rel="stylesheet" type="text/css" href="../Form/style.css">
 </head>
 <body>
-	
-	<?php include 'zodiac.php'; ?>
-
-	<div>
-		<div>
-			<br><h1 class="blueText center">Zodiac Sign Lookup</h1><br>
+	<div class="vertical">
+		<div class="center">
+			<strong><?php include 'zodiac.php'; ?></strong>
 		</div>
-		<section class="center">
-			<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>">
-				<label for="month">Month: </label>
-				<input type="number" name="month" min="1" max="12" style="width: 50%;"><br>
-				<label for="day">Day: </label>
-				<input type="number" name="day" style="width: 50%;margin-left: 22px;"><br>
-				<input type="submit" name="submit" value="Select" class="button">
-			</form>
-		</section>
-</div>
+		<div>
+			<div>
+				<br><h1 class="blueText center">Zodiac Sign Lookup</h1><br>
+			</div>
+			<section class="center">
+				<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>">
+					<label for="month">Month: </label>
+					<input type="number" name="month" min="1" max="12" style="width: 50%;" ><br>
+					<label for="day">Day: </label>
+					<input type="number" name="day" style="width: 50%;margin-left: 22px;" ><br>
+					<input type="submit" name="submit" value="Select" class="button">
+				</form>
+			</section>
+		</div>
+	</div>
 </body>
 </html>
